5ef1b1aba4e63c185bb34e46f636f302a4202ed7
Henauxg <19689618+Henauxg@users.noreply.github.com>
2020-09-08 19:06:13 +0200

    Line wrap

--- src/render/vlk/VlkDebug.hpp
+++ src/render/vlk/VlkDebug.hpp
@@ -6,42 +6,49 @@
 
 #if defined(__ANDROID__)
 /* TODO Update this. */
-#define ASSERT_VK_RESULT(f, message)                                                          \
-	{                                                                                         \
-		VkResult res = (f);                                                                   \
-		if (res != VK_SUCCESS) {                                                              \
-			LOGE("Fatal : VkResult is \" %s \" in %s at line %d",                             \
-				 vlk::vkResultToString(res).c_str(), __FILE__, __LINE__);                     \
-			abort();                                                                          \
-		}                                                                                     \
+#define ASSERT_VK_RESULT(f, message)                                      \
+	{                                                                     \
+		VkResult res = (f);                                               \
+		if (res != VK_SUCCESS)                                            \
+		{                                                                 \
+			LOGE("Fatal : VkResult is \" %s \" in %s at line %d",         \
+				 vlk::vkResultToString(res).c_str(), __FILE__, __LINE__); \
+			abort();                                                      \
+		}                                                                 \
 	}
-#define ASSERT_RESULT(f, message)                                                             \
-	{                                                                                         \
-		bool expr = (f);                                                                      \
-		if (!expr) {                                                                          \
-			LOGE("Fatal : assert failed in %s at line %d", __FILE__, __LINE__);               \
-			abort();                                                                          \
-		}                                                                                     \
+#define ASSERT_RESULT(f, message)                                         \
+	{                                                                     \
+		bool expr = (f);                                                  \
+		if (!expr)                                                        \
+		{                                                                 \
+			LOGE("Fatal : assert failed in %s at line %d", __FILE__,      \
+				 __LINE__);                                               \
+			abort();                                                      \
+		}                                                                 \
 	}
 #else
-#define ASSERT_VK_RESULT(f, message)                                                          \
-	{                                                                                         \
-		vk::Result res = (f);                                                                 \
-		if (res != vk::Result::eSuccess) {                                                    \
-			std::cout << "Fatal : VkResult is \"" << vlk::vkResultToString(res) << "\" in "   \
-					  << __FILE__ << " at line " << __LINE__ << ", message : " << message     \
-					  << std::endl;                                                           \
-			abort();                                                                          \
-		}                                                                                     \
+#define ASSERT_VK_RESULT(f, message)                                      \
+	{                                                                     \
+		vk::Result res = (f);                                             \
+		if (res != vk::Result::eSuccess)                                  \
+		{                                                                 \
+			std::cout << "Fatal : VkResult is \""                         \
+					  << vlk::vkResultToString(res) << "\" in "           \
+					  << __FILE__ << " at line " << __LINE__              \
+					  << ", message : " << message << std::endl;          \
+			abort();                                                      \
+		}                                                                 \
 	}
-#define ASSERT_RESULT(f, message)                                                             \
-	{                                                                                         \
-		bool expr = (f);                                                                      \
-		if (!expr) {                                                                          \
-			std::cout << "Fatal : assert failed in " << __FILE__ << " at line " << __LINE__   \
-					  << ", message : " << message << std::endl;                              \
-			abort();                                                                          \
-		}                                                                                     \
+#define ASSERT_RESULT(f, message)                                         \
+	{                                                                     \
+		bool expr = (f);                                                  \
+		if (!expr)                                                        \
+		{                                                                 \
+			std::cout << "Fatal : assert failed in " << __FILE__          \
+					  << " at line " << __LINE__                          \
+					  << ", message : " << message << std::endl;          \
+			abort();                                                      \
+		}                                                                 \
 	}
 #endif
 
@@ -66,15 +73,18 @@ const bool ENABLE_VALIDATION_LAYERS = false;
 const bool ENABLE_VALIDATION_LAYERS = true;
 #endif
 
-const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+const std::vector<const char*> validationLayers
+	= { "VK_LAYER_KHRONOS_validation" };
 
-bool hasValidationLayerSupport(const std::vector<const char*> validationLayers);
+bool hasValidationLayerSupport(
+	const std::vector<const char*> validationLayers);
 
 vk::DebugUtilsMessengerCreateInfoEXT
 getDebugUtilsCreateInfo(vk::DebugUtilsMessengerCreateInfoEXT& createInfo);
-vk::DebugUtilsMessengerEXT createDebugUtilsMessengerEXT(vk::Instance instance);
-void destroyDebugUtilsMessengerEXT(vk::Instance instance,
-								   vk::DebugUtilsMessengerEXT debugMessenger);
+vk::DebugUtilsMessengerEXT
+createDebugUtilsMessengerEXT(vk::Instance instance);
+void destroyDebugUtilsMessengerEXT(
+	vk::Instance instance, vk::DebugUtilsMessengerEXT debugMessenger);
 
 std::string vkResultToString(vk::Result errorCode);
 
